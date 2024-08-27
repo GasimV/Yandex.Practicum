4e1c65f08cb2a65deabad6935925f87b90981327
Gasym Valiyev <outnesss@yandex.ru>
2024-08-27 10:36:36 +0400

    Added PrintRange function!

---
+++ C++ Development/my-cpp-project/PrintRange.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (It i = range_begin; i != range_end; ++i) {
+        cout << *i << " "s;
+    }
+    cout << endl; // Вывести новую строку после печати всех элементов
+}
+
+int main() {
+    cout << "Test1"s << endl;
+    set<int> test1 = {1, 1, 1, 2, 3, 4, 5, 5};
+    PrintRange(test1.begin(), test1.end());
+    cout << "Test2"s << endl;
+    vector<int> test2 = {}; // пустой контейнер
+    PrintRange(test2.begin(), test2.end());
+    cout << "End of tests"s << endl;
+}
