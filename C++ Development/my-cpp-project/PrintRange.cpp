be09d25d2ee9211e851f937aeaa6cceed911649b
Gasym Valiyev <outnesss@yandex.ru>
2024-08-27 17:49:54 +0400

    Added EraseAndPrint function

--- C++ Development/my-cpp-project/PrintRange.cpp
+++ C++ Development/my-cpp-project/PrintRange.cpp
@@ -15,12 +15,20 @@ void PrintRange(It range_begin, It range_end) {
 }
 
 template <typename It>
-auto MakeSet(It range_begin, It range_end) {
-    return set(range_begin, range_end);
+auto MakeVector(It range_begin, It range_end) {
+    return vector(range_begin, range_end);
+}
+
+template <typename Container, typename Iterator>
+void EraseAndPrint(Container& container, Iterator it) {
+    // After erasing an element from the container, the iterator 'it' becomes invalid, so it should be reassigned
+    it = container.erase(it); // Erase the element and update 'it'
+    PrintRange(container.begin(), it);
+    PrintRange(it, container.end());
 }
 
 int main() {
-    vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s, "C++"s, "C++"s, "Ruby"s, "Java"s};
-    auto unique_langs = MakeSet(langs.begin(), langs.end());
-    PrintRange(unique_langs.begin(), unique_langs.end());
+    vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
+    EraseAndPrint(langs, langs.begin());
+    return 0;
 }
