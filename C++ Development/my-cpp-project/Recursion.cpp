8e2d646a48a07bf148aed50970bd4142a484dd70
Gasym Valiyev <outnesss@yandex.ru>
2024-08-28 14:11:48 +0400

    Added SolveHanoi Code

--- C++ Development/my-cpp-project/Recursion.cpp
+++ C++ Development/my-cpp-project/Recursion.cpp
@@ -1,20 +1,67 @@
 #include <iostream>
-#include <cstdint>
+#include <stdexcept>
+#include <vector>
 
 using namespace std;
 
-bool IsPowOfTwo(int n) {
-    if (n < 1) {
-        return false;
-    } else if (n == 1) {
-        return true;
-    } else if (n % 2 == 0) {
-        return IsPowOfTwo(n / 2);
+class Tower {
+public:
+    Tower(int disks_num) {
+        FillTower(disks_num);
     }
-    return false;
+
+    int GetDisksNum() const {
+        return disks_.size();
+    }
+
+    void SetDisks(int disks_num) {
+        FillTower(disks_num);
+    }
+
+    void AddToTop(int disk) {
+        int top_disk_num = disks_.size() - 1;
+        if (0 != disks_.size() && disk >= disks_[top_disk_num]) {
+            throw invalid_argument("Невозможно поместить большой диск на маленький");
+        } else {
+            disks_.push_back(disk);
+        }
+    }
+
+    void MoveDisks(int disks_num, Tower& destination, Tower& buffer) {
+        if (disks_num == 1) {
+            destination.AddToTop(disks_.back());
+            disks_.pop_back();
+        } else {
+            MoveDisks(disks_num - 1, buffer, destination);
+            destination.AddToTop(disks_.back());
+            disks_.pop_back();
+            buffer.MoveDisks(disks_num - 1, destination, *this);
+        }
+    }
+
+private:
+    vector<int> disks_;
+
+    void FillTower(int disks_num) {
+        for (int i = disks_num; i > 0; i--) {
+            disks_.push_back(i);
+        }
+    }
+};
+
+void SolveHanoi(vector<Tower>& towers) {
+    int disks_num = towers[0].GetDisksNum();
+    towers[0].MoveDisks(disks_num, towers[2], towers[1]);
 }
 
 int main() {
-    int result = IsPowOfTwo(1024);
-    cout << result << endl;
+    int towers_num = 3;
+    int disks_num = 3;
+    vector<Tower> towers;
+    for (int i = 0; i < towers_num; ++i) {
+        towers.push_back(0);
+    }
+    towers[0].SetDisks(disks_num);
+
+    SolveHanoi(towers);
 }
