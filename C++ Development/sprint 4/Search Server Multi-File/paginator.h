5a0b8a92b69a1e71b065179b879bb128fe0e8dbe
Gasym Valiyev <outnesss@yandex.ru>
2024-09-04 14:16:53 +0400

    Changed the paginator.h - unsuccsessful

--- C++ Development/sprint 4/Search Server Multi-File/paginator.h
+++ C++ Development/sprint 4/Search Server Multi-File/paginator.h
@@ -3,20 +3,22 @@
 #include <vector>
 #include <iterator>
 #include <iostream>
+#include <cassert>  // Include assert for using assert()
 
 template <typename Iterator>
 class IteratorRange {
 private:
     Iterator range_begin;
     Iterator range_end;
+    size_t range_size;
 
 public:
     IteratorRange(Iterator begin, Iterator end)
-        : range_begin(begin), range_end(end) {}
+        : range_begin(begin), range_end(end), range_size(std::distance(begin, end)) {}
 
     Iterator begin() const { return range_begin; }
     Iterator end() const { return range_end; }
-    size_t size() const { return std::distance(range_begin, range_end); }
+    size_t size() const { return range_size; }
 };
 
 template <typename Iterator>
@@ -34,9 +36,16 @@ private:
 
 public:
     Paginator(Iterator range_begin, Iterator range_end, size_t page_size) {
+        assert(page_size > 0);  // Ensure page size is positive
+
+        // Check if range_begin equals range_end (empty container case)
+        if (range_begin == range_end) {
+            return;  // No pages to be created if the range is empty
+        }
+
         for (Iterator it = range_begin; it != range_end;) {
-            Iterator next_it = it;
-            std::advance(next_it, std::min(page_size, static_cast<size_t>(std::distance(it, range_end))));
+            size_t current_page_size = std::min(page_size, static_cast<size_t>(std::distance(it, range_end)));
+            Iterator next_it = std::next(it, current_page_size);  // Advance safely by page size
             pages.push_back({it, next_it});
             it = next_it;
         }
