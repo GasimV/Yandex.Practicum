1d9eb0fed27603010b20562f2f896b627a66dddc
Gasym Valiyev <outnesss@yandex.ru>
2024-09-10 22:13:36 +0400

    Optimized vector reverse functions and random number generation for better performance

--- C++ Development/sprint 5/main.cpp
+++ C++ Development/sprint 5/main.cpp
@@ -10,22 +10,22 @@ using namespace std;
 // Наивная реализация реверсирования
 vector<int> ReverseVector(const vector<int>& source_vector) {
     vector<int> res;
+    res.reserve(source_vector.size());  // Резервируем память
 
     for (int i : source_vector) {
-        res.insert(res.begin(), i);
+        res.push_back(i);
     }
-
+    reverse(res.begin(), res.end());  // Реверсируем вектор одной строкой
     return res;
 }
 
 // Хорошая реализация реверсирования (вставки в конец)
 vector<int> ReverseVector2(const vector<int>& source_vector) {
     vector<int> res;
+    res.reserve(source_vector.size());  // Резервируем память
 
-    // будем проходить source_vector задом наперёд
-    // с помощью обратного итератора
-    for (auto iterator = source_vector.rbegin(); iterator != source_vector.rend(); ++iterator) {
-        res.push_back(*iterator);
+    for (auto it = source_vector.rbegin(); it != source_vector.rend(); ++it) {
+        res.push_back(*it);
     }
 
     return res;
@@ -33,7 +33,7 @@ vector<int> ReverseVector2(const vector<int>& source_vector) {
 
 // Отличная реализация реверсирования (двумя итераторами)
 vector<int> ReverseVector3(const vector<int>& source_vector) {
-    return {source_vector.rbegin(), source_vector.rend()};
+    return {source_vector.rbegin(), source_vector.rend()};  // Реверсируем с помощью итераторов
 }
 
 // Собственная реализация реверсирования (заполняем с конца)
@@ -47,18 +47,40 @@ vector<int> ReverseVector4(const vector<int>& source_vector) {
     return res;
 }
 
+// Оптимизация функции AppendRandom: уменьшение вызовов rand в 15 раз
+void AppendRandom(vector<int>& v, int n) {
+    v.reserve(n);  // Резервируем память для n элементов
+
+    for (int i = 0; i < n; i += 15) {
+        int number = rand();  // Генерируем одно случайное число
+        for (int j = 0; j < 15 && i + j < n; ++j) {
+            v.push_back((number >> j) % 2);  // Извлекаем биты случайного числа
+        }
+    }
+}
+
+// Функция CountPops для подсчёта единичных элементов в диапазоне
+int CountPops(const vector<int>& source_vector, int begin, int end) {
+    int res = 0;
+    for (int i = begin; i < end; ++i) {
+        if (source_vector[i] == 1) {
+            ++res;
+        }
+    }
+    return res;
+}
+
 // Функция для запуска тестов и замеров
 void Operate() {
     vector<int> rand_vector;
     int n;
 
+    // Чтение размера вектора
     cin >> n;
     rand_vector.reserve(n);
 
-    // Заполняем вектор случайными числами
-    for (int i = 0; i < n; ++i) {
-        rand_vector.push_back(rand());
-    }
+    // Заполнение вектора случайными числами
+    AppendRandom(rand_vector, n);
 
     // Если размер вектора <= 100000, тестируем Naive и Good
     if (n <= 100'000) {
@@ -70,7 +92,7 @@ void Operate() {
             LOG_DURATION("Good");
             auto reversed = ReverseVector2(rand_vector);
         }
-    }
+    } 
     // Если размер вектора > 100000, тестируем Good, Best и Your
     else {
         {
