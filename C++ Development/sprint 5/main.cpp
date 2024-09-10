91c7c2dd2607fcf30aee98ad1ae0ad3084cc7ee8
Gasym Valiyev <outnesss@yandex.ru>
2024-09-10 23:59:17 +0400

    Implemented ComputeAvgTemp to calculate average positive temperatures efficiently

--- C++ Development/sprint 5/main.cpp
+++ C++ Development/sprint 5/main.cpp
@@ -1,116 +1,76 @@
 #include "log_duration.h"
 
-#include <algorithm>
-#include <cstdlib>
 #include <iostream>
+#include <numeric>
+#include <random>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-// Наивная реализация реверсирования
-vector<int> ReverseVector(const vector<int>& source_vector) {
-    vector<int> res;
-    res.reserve(source_vector.size());  // Резервируем память
-
-    for (int i : source_vector) {
-        res.push_back(i);
+vector<float> ComputeAvgTemp(const vector<vector<float>>& measures) {
+    if (measures.empty()) {
+        return {}; // Если пустой набор, возвращаем пустой вектор
     }
-    reverse(res.begin(), res.end());  // Реверсируем вектор одной строкой
+
+    // int days = measures.size();
+    int measurements_per_day = measures[0].size();
+
+    // Вектор для суммы положительных температур
+    vector<float> sum(measurements_per_day, 0.0f);
+    // Вектор для подсчета положительных измерений
+    vector<int> count(measurements_per_day, 0);
+
+    // Проход по каждому дню
+    for (const auto& day : measures) {
+        // Проход по каждому измерению
+        for (int i = 0; i < measurements_per_day; ++i) {
+            // Если температура положительная, добавляем её в сумму и увеличиваем счетчик
+            sum[i] += (day[i] > 0 ? day[i] : 0);
+            count[i] += (day[i] > 0 ? 1 : 0);
+        }
+    }
+
+    // Вектор для хранения среднего значения температур
+    vector<float> avg(measurements_per_day, 0.0f);
+
+    // Вычисляем среднее значение для каждого времени измерения
+    for (int i = 0; i < measurements_per_day; ++i) {
+        if (count[i] > 0) {
+            avg[i] = sum[i] / count[i]; // Делим сумму на количество положительных значений
+        } else {
+            avg[i] = 0.0f; // Если не было положительных измерений, записываем 0
+        }
+    }
+
+    return avg;
+}
+
+vector<float> GetRandomVector(int size) {
+    static mt19937 engine;
+    uniform_real_distribution<float> d(-100, 100);
+
+    vector<float> res(size);
+    for (int i = 0; i < size; ++i) {
+        res[i] = d(engine);
+    }
+
     return res;
-}
-
-// Хорошая реализация реверсирования (вставки в конец)
-vector<int> ReverseVector2(const vector<int>& source_vector) {
-    vector<int> res;
-    res.reserve(source_vector.size());  // Резервируем память
-
-    for (auto it = source_vector.rbegin(); it != source_vector.rend(); ++it) {
-        res.push_back(*it);
-    }
-
-    return res;
-}
-
-// Отличная реализация реверсирования (двумя итераторами)
-vector<int> ReverseVector3(const vector<int>& source_vector) {
-    return {source_vector.rbegin(), source_vector.rend()};  // Реверсируем с помощью итераторов
-}
-
-// Собственная реализация реверсирования (заполняем с конца)
-vector<int> ReverseVector4(const vector<int>& source_vector) {
-    vector<int> res(source_vector.size());
-
-    for (size_t i = 0; i < source_vector.size(); ++i) {
-        res[source_vector.size() - 1 - i] = source_vector[i];
-    }
-
-    return res;
-}
-
-// Оптимизация функции AppendRandom: уменьшение вызовов rand в 15 раз
-void AppendRandom(vector<int>& v, int n) {
-    v.reserve(n);  // Резервируем память для n элементов
-
-    for (int i = 0; i < n; i += 15) {
-        int number = rand();  // Генерируем одно случайное число
-        for (int j = 0; j < 15 && i + j < n; ++j) {
-            v.push_back((number >> j) % 2);  // Извлекаем биты случайного числа
-        }
-    }
-}
-
-// Функция CountPops для подсчёта единичных элементов в диапазоне
-int CountPops(const vector<int>& source_vector, int begin, int end) {
-    int res = 0;
-    for (int i = begin; i < end; ++i) {
-        if (source_vector[i] == 1) {
-            ++res;
-        }
-    }
-    return res;
-}
-
-// Функция для запуска тестов и замеров
-void Operate() {
-    vector<int> rand_vector;
-    int n;
-
-    // Чтение размера вектора
-    cin >> n;
-    rand_vector.reserve(n);
-
-    // Заполнение вектора случайными числами
-    AppendRandom(rand_vector, n);
-
-    // Если размер вектора <= 100000, тестируем Naive и Good
-    if (n <= 100'000) {
-        {
-            LOG_DURATION("Naive");
-            auto reversed = ReverseVector(rand_vector);
-        }
-        {
-            LOG_DURATION("Good");
-            auto reversed = ReverseVector2(rand_vector);
-        }
-    } 
-    // Если размер вектора > 100000, тестируем Good, Best и Your
-    else {
-        {
-            LOG_DURATION("Good");
-            auto reversed = ReverseVector2(rand_vector);
-        }
-        {
-            LOG_DURATION("Best");
-            auto reversed = ReverseVector3(rand_vector);
-        }
-        {
-            LOG_DURATION("Your");
-            auto reversed = ReverseVector4(rand_vector);
-        }
-    }
 }
 
 int main() {
-    Operate();
-    return 0;
+    vector<vector<float>> data;
+    data.reserve(5000);
+
+    for (int i = 0; i < 5000; ++i) {
+        data.push_back(GetRandomVector(5000));
+    }
+
+    vector<float> avg;
+    {
+        LOG_DURATION("ComputeAvgTemp"s);
+        avg = ComputeAvgTemp(data);
+    }
+
+    cout << "Total mean: "s << accumulate(avg.begin(), avg.end(), 0.f) / avg.size() << endl;
 }
