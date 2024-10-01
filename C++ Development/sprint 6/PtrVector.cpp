f712c8128e9bb8e19de3b377cd1e94a4befe65c6
Gasym Valiyev <outnesss@yandex.ru>
2024-10-01 06:15:27 +0400

    Implemented the copy assignment operator using the copy-and-swap idiom to ensure that PtrVector provides strong exception safety and works correctly in all scenarios, including self-assignment.

--- C++ Development/sprint 6/PtrVector.cpp
+++ C++ Development/sprint 6/PtrVector.cpp
@@ -1,115 +1,186 @@
 #include <algorithm>
 #include <cassert>
+#include <stdexcept>
 #include <vector>
 
 using namespace std;
 
+// Реализация PtrVector с оператором присваивания копии с использованием идиомы copy-and-swap
 template <typename T>
 class PtrVector {
 public:
     PtrVector() = default;
 
-    // Copy constructor
+    // Создаёт вектор указателей на копии объектов из other
     PtrVector(const PtrVector& other) {
-        items_.reserve(other.items_.size()); // Reserve memory to prevent exceptions during copying
-        for (T* item : other.items_) {
-            if (item) {
-                items_.push_back(new T(*item)); // Deep copy each non-null pointer
-            } else {
-                items_.push_back(nullptr); // Keep nullptrs as is
+        // Резервируем место в vector-е для хранения нужного количества элементов
+        // Благодаря этому при push_back не будет выбрасываться исключение
+        items_.reserve(other.items_.size());
+
+        try {
+            for (auto p : other.items_) {
+                // Копируем объект, если указатель на него ненулевой
+                auto p_copy = p ? new T(*p) : nullptr;  // new может выбросить исключение
+
+                // Не выбросит исключение, т. к. в vector память уже зарезервирована
+                items_.push_back(p_copy);
             }
+        } catch (...) {
+            // удаляем элементы в векторе и перевыбрасываем пойманное исключение
+            DeleteItems();
+            throw;
         }
     }
 
-    // Destructor
+    // Деструктор удаляет объекты в куче, на которые ссылаются указатели,
+    // в векторе items_
     ~PtrVector() {
-        for (T* item : items_) {
-            delete item; // Delete each object pointed to
+        DeleteItems();
+    }
+    
+    // Оператор присваивания копии с использованием идиомы copy-and-swap
+    PtrVector& operator=(const PtrVector& rhs) {
+        if (this != &rhs) {
+            PtrVector temp(rhs); // Создаём копию rhs
+            swap(temp); // Поменяем местами this с temp
         }
+        return *this;
     }
 
-    // Returns a non-const reference to the vector of pointers
+    // Метод для обмена содержимого двух объектов PtrVector
+    void swap(PtrVector& other) noexcept {
+        items_.swap(other.items_); // Меняем векторы местами; эта операция не вызывает исключений
+    }
+
+    // Возвращает ссылку на вектор указателей
     vector<T*>& GetItems() noexcept {
         return items_;
     }
 
-    // Returns a const reference to the vector of pointers
+    // Возвращает константную ссылку на вектор указателей
     vector<T*> const& GetItems() const noexcept {
         return items_;
     }
 
 private:
+    void DeleteItems() noexcept {
+        for (auto p : items_) {
+            delete p;
+        }
+    }
+
     vector<T*> items_;
 };
 
-// This main function tests the PtrVector template class
+// Перегружаем функцию swap, чтобы разрешить использование std::swap
+template <typename T>
+void swap(PtrVector<T>& lhs, PtrVector<T>& rhs) noexcept {
+    lhs.swap(rhs);
+}
+
+// Эта функция main тестирует шаблон класса PtrVector
 int main() {
-    // Helper "spy" struct that helps track its deletion
-    struct DeletionSpy {
-        explicit DeletionSpy(bool& is_deleted)
-            : is_deleted_(is_deleted) {
-        }
-        ~DeletionSpy() {
-            is_deleted_ = true;
-        }
-        bool& is_deleted_;
-    };
-
-    // Test element deletion
-    {
-        bool spy1_is_deleted = false;
-        DeletionSpy* ptr1 = new DeletionSpy(spy1_is_deleted);
-        {
-            PtrVector<DeletionSpy> ptr_vector;
-            ptr_vector.GetItems().push_back(ptr1);
-            assert(!spy1_is_deleted);
-
-            // Constant reference to ptr_vector
-            const auto& const_ptr_vector_ref(ptr_vector);
-            // Both const and non-const versions of GetItems should return a reference to the same vector
-            assert(&const_ptr_vector_ref.GetItems() == &ptr_vector.GetItems());
-        }
-        // The PtrVector destructor should delete all objects the pointers point to
-        assert(spy1_is_deleted);
-    }
-
-    // Helper "spy" struct that tracks its copying
     struct CopyingSpy {
-        explicit CopyingSpy(int& copy_count)
-            : copy_count_(copy_count) {
+        CopyingSpy(int& copy_count, int& deletion_count)
+            : copy_count_(copy_count)
+            , deletion_count_(deletion_count) {
         }
         CopyingSpy(const CopyingSpy& rhs)
-            : copy_count_(rhs.copy_count_)  //
+            : copy_count_(rhs.copy_count_)          // счётчик копирований
+            , deletion_count_(rhs.deletion_count_)  // счётчик удалений
         {
+            if (rhs.throw_on_copy_) {
+                throw runtime_error("copy construction failed"s);
+            }
             ++copy_count_;
         }
+        ~CopyingSpy() {
+            ++deletion_count_;
+        }
+        void ThrowOnCopy() {
+            throw_on_copy_ = true;
+        }
+
+    private:
         int& copy_count_;
+        int& deletion_count_;
+        bool throw_on_copy_ = false;
     };
 
-    // Test copying elements when copying a vector of pointers
+    // Проверка присваивания
     {
-        // 10 elements
-        vector<int> copy_counters(10);
+        int item0_copy_count = 0;
+        int item0_deletion_count = 0;
+        {
+            PtrVector<CopyingSpy> v;
 
-        PtrVector<CopyingSpy> ptr_vector;
-        // Prepare the original array of pointers
-        for (auto& counter : copy_counters) {
-            ptr_vector.GetItems().push_back(new CopyingSpy(counter));
+            v.GetItems().push_back(new CopyingSpy(item0_copy_count, item0_deletion_count));
+            v.GetItems().push_back(nullptr);
+            {
+                PtrVector<CopyingSpy> v_copy;
+                v_copy = v;
+                assert(v_copy.GetItems().size() == v.GetItems().size());
+                assert(v_copy.GetItems().at(0) != v.GetItems().at(0));
+                assert(v_copy.GetItems().at(1) == nullptr);
+                assert(item0_copy_count == 1);
+                assert(item0_deletion_count == 0);
+            }
+            assert(item0_deletion_count == 1);
         }
-        // The last element contains a nullptr
-        ptr_vector.GetItems().push_back(nullptr);
+        assert(item0_deletion_count == 2);
+    }
 
-        auto ptr_vector_copy(ptr_vector);
-        // The number of elements in the copy should be equal to the number of elements in the original vector
-        assert(ptr_vector_copy.GetItems().size() == ptr_vector.GetItems().size());
+    // Проверка корректности самоприсваивания
+    {
+        int item0_copy_count = 0;
+        int item0_deletion_count = 0;
 
-        // The copy should store pointers to new objects
-        assert(ptr_vector_copy.GetItems() != ptr_vector.GetItems());
-        // The last element of the original array and its copy should be a nullptr
-        assert(ptr_vector_copy.GetItems().back() == nullptr);
-        // Check that elements were copied (the copying spies should increment copy counters)
-        assert(all_of(copy_counters.begin(), copy_counters.end(), [](int counter) {
-            return counter == 1;
-        }));
+        PtrVector<CopyingSpy> v;
+        v.GetItems().push_back(new CopyingSpy(item0_copy_count, item0_deletion_count));
+        CopyingSpy* first_item = v.GetItems().front();
+
+        v = v;
+        assert(v.GetItems().size() == 1);
+        // При самоприсваивании объекты должны быть расположены по тем же адресам
+        assert(v.GetItems().front() == first_item);
+        assert(item0_copy_count == 0);
+        assert(item0_deletion_count == 0);
+    }
+
+    // Проверка обеспечения строгой гарантии безопасности исключений при присваивании
+    {
+        int item0_copy_count = 0;
+        int item0_deletion_count = 0;
+
+        int item1_copy_count = 0;
+        int item1_deletion_count = 0;
+
+        // v хранит 2 элемента
+        PtrVector<CopyingSpy> v;
+        v.GetItems().push_back(new CopyingSpy(item0_copy_count, item0_deletion_count));
+        v.GetItems().push_back(new CopyingSpy(item1_copy_count, item1_deletion_count));
+
+        int other_item0_copy_count = 0;
+        int other_item0_deletion_count = 0;
+        // other_vector хранит 1 элемент, при копировании которого будет выброшено исключение
+        PtrVector<CopyingSpy> other_vector;
+        other_vector.GetItems().push_back(new CopyingSpy(other_item0_copy_count, other_item0_deletion_count));
+        other_vector.GetItems().front()->ThrowOnCopy();
+
+        // Сохраняем массив указателей
+        auto v_items(v.GetItems());
+
+        try {
+            v = other_vector;
+            // Операция должна выбросить исключение
+            assert(false);
+        } catch (const runtime_error&) {
+        }
+
+        // Элементы массива должны остаться прежними
+        assert(v.GetItems() == v_items);
+        assert(item0_copy_count == 0);
+        assert(item1_copy_count == 0);
+        assert(other_item0_copy_count == 0);
     }
 }
