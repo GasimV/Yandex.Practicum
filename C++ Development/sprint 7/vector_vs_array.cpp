15a9aa795d58cb01beb4c4d78c166c925ed0f9bf
Gasym Valiyev <outnesss@yandex.ru>
2024-10-11 11:13:59 +0400

    Compared std::vector and std::array performance

---
+++ C++ Development/sprint 7/vector_vs_array.cpp
@@ -0,0 +1,65 @@
+#include <chrono>
+#include <iostream>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+#define PROFILE_CONCAT_INTERNAL(X, Y) X##Y
+#define PROFILE_CONCAT(X, Y) PROFILE_CONCAT_INTERNAL(X, Y)
+#define UNIQUE_VAR_NAME_PROFILE PROFILE_CONCAT(profileGuard, __LINE__)
+#define LOG_DURATION(x) LogDuration UNIQUE_VAR_NAME_PROFILE(x)
+#define LOG_DURATION_STREAM(x, y) LogDuration UNIQUE_VAR_NAME_PROFILE(x, y)
+
+class LogDuration {
+public:
+    // заменим имя типа std::chrono::steady_clock
+    // с помощью using для удобства
+    using Clock = std::chrono::steady_clock;
+
+    LogDuration(const std::string& id, std::ostream& dst_stream = std::cerr)
+        : id_(id)
+        , dst_stream_(dst_stream) {
+    }
+
+    ~LogDuration() {
+        using namespace std::chrono;
+        using namespace std::literals;
+
+        const auto end_time = Clock::now();
+        const auto dur = end_time - start_time_;
+        dst_stream_ << id_ << ": "s << duration_cast<milliseconds>(dur).count() << " ms"s << std::endl;
+    }
+
+private:
+    const std::string id_;
+    const Clock::time_point start_time_ = Clock::now();
+    std::ostream& dst_stream_;
+};
+
+vector<int> BuildVector(int i) {
+    return {i, i + 1, i + 2, i + 3, i + 4};
+}
+
+array<int, 5> BuildArray(int i) {
+    return {i, i + 1, i + 2, i + 3, i + 4};
+}
+
+const int COUNT = 1000000;
+
+int main() {
+    {
+        LOG_DURATION("vector");
+        for (int i = 0; i < COUNT; ++i) {
+            BuildVector(i);
+        }
+    }
+    {
+        LOG_DURATION("array");
+        for (int i = 0; i < COUNT; ++i) {
+            BuildArray(i);
+        }
+    }
+
+    return 0;
+}
