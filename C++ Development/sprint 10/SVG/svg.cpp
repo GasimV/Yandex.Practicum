a6c561ea3e96d8a355d04769e9cdca6b9c9cfc99
Gasym Valiyev <outnesss@yandex.ru>
2024-11-30 09:53:50 +0400

    Implement full SVG library including Circle, Polyline, Text, and Document classes with rendering support

---
+++ C++ Development/sprint 10/SVG/svg.cpp
@@ -0,0 +1,122 @@
+#include "svg.h"
+
+namespace svg {
+
+using namespace std::literals;
+
+void Object::Render(const RenderContext& context) const {
+    context.RenderIndent();
+
+    // Делегируем вывод тега своим подклассам
+    RenderObject(context);
+
+    context.out << std::endl;
+}
+
+// ---------- Circle ------------------
+
+Circle& Circle::SetCenter(Point center)  {
+    center_ = center;
+    return *this;
+}
+
+Circle& Circle::SetRadius(double radius)  {
+    radius_ = radius;
+    return *this;
+}
+
+void Circle::RenderObject(const RenderContext& context) const {
+    auto& out = context.out;
+    out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
+    out << "r=\""sv << radius_ << "\" />"sv;
+}
+
+Polyline& Polyline::AddPoint(Point point) {
+    points_.emplace_back(point);
+    return *this;
+}
+
+void Polyline::RenderObject(const RenderContext& context) const {
+    auto& out = context.out;
+    out << "<polyline points=\""sv;
+    bool first = true;
+    for (const auto& point : points_) {
+        if (!first) {
+            out << " ";
+        }
+        first = false;
+        out << point.x << "," << point.y;
+    }
+    out << "\" />"sv;
+}
+
+Text& Text::SetPosition(Point pos) {
+    position_ = pos;
+    return *this;
+}
+
+Text& Text::SetOffset(Point offset) {
+    offset_ = offset;
+    return *this;
+}
+
+Text& Text::SetFontSize(uint32_t size) {
+    font_size_ = size;
+    return *this;
+}
+
+Text& Text::SetFontFamily(std::string font_family) {
+    font_family_ = std::move(font_family);
+    return *this;
+}
+
+Text& Text::SetFontWeight(std::string font_weight) {
+    font_weight_ = std::move(font_weight);
+    return *this;
+}
+
+Text& Text::SetData(std::string data) {
+    data_ = std::move(data);
+    return *this;
+}
+
+void Text::RenderObject(const RenderContext& context) const {
+    auto& out = context.out;
+    out << "<text x=\""sv << position_.x << "\" y=\""sv << position_.y << "\""sv;
+    out << " dx=\""sv << offset_.x << "\" dy=\""sv << offset_.y << "\""sv;
+    out << " font-size=\""sv << font_size_ << "\""sv;
+    if (!font_family_.empty()) {
+        out << " font-family=\""sv << font_family_ << "\""sv;
+    }
+    if (!font_weight_.empty()) {
+        out << " font-weight=\""sv << font_weight_ << "\""sv;
+    }
+    out << ">"sv;
+    for (char c : data_) {
+        switch (c) {
+            case '<': out << "&lt;"sv; break;
+            case '>': out << "&gt;"sv; break;
+            case '"': out << "&quot;"sv; break;
+            case '&': out << "&amp;"sv; break;
+            case '\'': out << "&apos;"sv; break;
+            default: out << c;
+        }
+    }
+    out << "</text>"sv;
+}
+
+void Document::AddPtr(std::unique_ptr<Object>&& obj) {
+    objects_.emplace_back(std::move(obj));
+}
+
+void Document::Render(std::ostream& out) const {
+    out << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n"sv;
+    out << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">\n"sv;
+    RenderContext context(out, 2, 2);
+    for (const auto& obj : objects_) {
+        obj->Render(context);
+    }
+    out << "</svg>"sv;
+}
+
+}  // namespace svg
