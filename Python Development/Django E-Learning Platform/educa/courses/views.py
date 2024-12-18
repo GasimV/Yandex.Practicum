6b99cf5fdeee33792cf4ca517d69c0f5e3826762
Gasym Valiyev <outnesss@yandex.ru>
2024-12-18 15:03:52 +0400

    Created a common interface for rendering courses with different types of content.

--- Python Development/Django E-Learning Platform/educa/courses/views.py
+++ Python Development/Django E-Learning Platform/educa/courses/views.py
@@ -15,6 +15,7 @@ from django.views.generic.detail import DetailView
 
 from .forms import ModuleFormSet
 from .models import Content, Course, Module, Subject
+from students.forms import CourseEnrollForm
 
 
 class OwnerMixin:
@@ -210,4 +211,10 @@ class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+            initial={'course': self.object}
+        )
+        return context
 
