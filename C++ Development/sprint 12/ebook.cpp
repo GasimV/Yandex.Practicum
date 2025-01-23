88ac51d81c0eee80511a81b98401b1c452ade90b
Gasym Valiyev <outnesss@yandex.ru>
2025-01-23 19:47:30 +0400

    Added Comments.

--- C++ Development/sprint 12/ebook.cpp
+++ C++ Development/sprint 12/ebook.cpp
@@ -4,36 +4,48 @@
 
 using namespace std;
 
-const int MAX_USERS = 100000;
-const int MAX_PAGES = 1000;
+const int MAX_USERS = 100000;  // Максимальное количество пользователей
+const int MAX_PAGES = 1000;    // Максимальное количество страниц в книге
 
+// Вектор для хранения прогресса каждого пользователя по страницам
 vector<int> user_progress(MAX_USERS + 1, 0);
+
+// Вектор для хранения количества пользователей, дочитавших как минимум до страницы m
 vector<int> page_count(MAX_PAGES + 1, 0);
 
+// Функция обработки команды READ
 void process_read(int user, int page) {
+    // Если пользователь уже дочитал до определенной страницы, уменьшаем счетчик
     if (user_progress[user] > 0) {
         page_count[user_progress[user]]--;
     }
+    // Обновляем прогресс пользователя и увеличиваем счетчик для новой страницы
     user_progress[user] = page;
     page_count[page]++;
 }
 
+// Функция обработки команды CHEER
+// Возвращает долю пользователей, прочитавших меньше страниц, чем данный пользователь
 double process_cheer(int user) {
+    // Если пользователь еще не читал книгу, возвращаем 0.0
     if (user_progress[user] == 0) {
         return 0.0;
     }
     int user_page = user_progress[user];
     int readers_ahead = 0, total_readers = 0;
     
+    // Подсчитываем общее количество читателей и тех, кто прочитал меньше страниц
     for (int i = 1; i <= MAX_PAGES; ++i) {
         total_readers += page_count[i];
         if (i < user_page) {
             readers_ahead += page_count[i];
         }
     }
+    // Если пользователь единственный, возвращаем 1.0
     if (total_readers <= 1) {
         return 1.0;
     }
+    // Вычисляем долю читателей, которые прочитали меньше
     return static_cast<double>(readers_ahead) / (total_readers - 1);
 }
 
