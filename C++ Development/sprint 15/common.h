42c79a27da678f422f656a2a3fda8363d27181eb
Gasym Valiyev <outnesss@yandex.ru>
2025-03-16 08:54:34 +0400

    Implemented the Evaluate() method for the BinaryOpExpr, UnaryOpExpr, and NumberExpr classes in the FormulaAST.cpp file. Now the code is able to correctly evaluate formulas, including handling division by zero and incorrect operations.

--- C++ Development/sprint 15/common.h
+++ C++ Development/sprint 15/common.h
@@ -1,23 +1,16 @@
 #pragma once
 
-#include <sstream>
-#include <string>
-#include <string_view>
+#include <stdexcept>
 
-// Позиция ячейки. Индексация с нуля.
-struct Position {
-    int row = 0;
-    int col = 0;
+// Описывает ошибки, которые могут возникнуть при вычислении формулы.
+class FormulaError : public std::runtime_error {
+public:
+    using std::runtime_error::runtime_error;
+};
 
-    bool operator==(Position rhs) const;
-    bool operator<(Position rhs) const;
-
-    bool IsValid() const;
-    std::string ToString() const;
-
-    static Position FromString(std::string_view str);
-
-    static const int MAX_ROWS = 16384;
-    static const int MAX_COLS = 16384;
-    static const Position NONE;
+// Исключение, выбрасываемое при попытке задать синтаксически некорректную
+// формулу.
+class FormulaException : public std::runtime_error {
+public:
+    using std::runtime_error::runtime_error;
 };
